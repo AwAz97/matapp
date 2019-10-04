f13af7cb16f8fd2e5bab9ff50cbd7db58e14238f
tare <tba.97@hotmail.com>
2019-10-04 21:38:16 +0200

    Searchview

--- Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/FinnOppskrift.java
+++ Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/FinnOppskrift.java
@@ -1,14 +1,64 @@
 package com.gmail.andreas.gautestad.matapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class FinnOppskrift extends AppCompatActivity {
+
+    ListView searchFood;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finn_oppskrift);
+
+        searchFood = (ListView) findViewById(R.id.searchFood);
+
+        ArrayList<String> arrayFood = new ArrayList<>();
+        arrayFood.addAll(Arrays.asList(getResources().getStringArray(R.array.myFood)));
+
+        adapter = new ArrayAdapter<String>(
+                FinnOppskrift.this,
+                android.R.layout.simple_list_item_1,
+                arrayFood
+        );
+
+        searchFood.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.searchFood);
+        SearchView searchView = (SearchView)item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
