83715fa0a7333c7cedc3add6a2df53badfcc0d12
AwAz <Andreas.gautestad@gmail.com>
2019-10-07 00:54:31 -0700

    DarkmodeNAF

--- Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/Favoritter.java
+++ Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/Favoritter.java
@@ -2,6 +2,8 @@ package com.gmail.andreas.gautestad.matapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class Favoritter extends AppCompatActivity {
@@ -10,5 +12,23 @@ public class Favoritter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favoritter);
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+
+        Context context = getApplicationContext();
+        SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.nattmodus), Context.MODE_PRIVATE);
+        String sjekk = sharedPref.getString("nattmodus", null);
+
+        if(sjekk.equals("Sann")) {
+            setContentView(R.layout.activity_favoritter_dark);
+        }
+        else{
+            setContentView(R.layout.activity_favoritter);
+        }
+
     }
 }
