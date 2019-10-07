e0e4fe7e6ca3f39909433c704afdcedd62cf1e0a
Nick <zick97@live.com>
2019-10-07 21:18:05 +0200

    Oppretter klasse for bruker og gjorde ferdig API login

--- Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/Bakgrunn.java
+++ Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/Bakgrunn.java
@@ -1,46 +1,75 @@
 package com.gmail.andreas.gautestad.matapp;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 
-public class Bakgrunn extends AsyncTask<String,Void,Void> {
+public class Bakgrunn extends AsyncTask<String,Void,String> {
     Context context;
-    BakgrunnJobb (Context ctx){
+    AlertDialog alertDialog;
+    Bakgrunn (Context ctx){
         context = ctx;
     }
     @Override
-    protected Void doInBackground(String... voids) {
+    protected String doInBackground(String... voids) {
+        String[] params;
         String type = params[0];
         String login_url = "https://web01.usn.no/~216728/api.php/records/brukere/?include=brukernavn,passord&filter=passord,eq" + passordVar + "&filter=brukernavn,eq," + brukernavnVar;
         if(type.equals("login")) {
             try {
+                String brukernavn = params[1];
+                String passord = params[2];
                 URL url = new URL(login_url);
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outpuStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outpuStream, "UTF-8"));
+                String post_data = URLEncoder.encode("brukernavn", "UTF-8")+"="URLEncoder.encode(brukernavn, "UTF-8")+"&"
+                        +URLEncoder.encode("passord", "UTF-8")+"="URLEncoder.encode(passord, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outpuStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bufferedReader.readLine())!= null){
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                //HttpURLConnection.di();
+                return result
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e){
-                a.printStackTrace();
+                e.printStackTrace();
             }
         }
         return null;
     }
 
-    @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
-    }
-    @Override
-    onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
-    }
 
     @Override
-    protected void onProgressUpdate(Void... values) {
-        super.onProgressUpdate(values);
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
     }
+
+
 }
--- Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/Login.java
+++ Matapp/app/src/main/java/com/gmail/andreas/gautestad/matapp/Login.java
@@ -2,31 +2,50 @@ package com.gmail.andreas.gautestad.matapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Objects;
 
 public class Login extends AppCompatActivity {
 
-    EditText brukernavn, passord;
+    EditText Brukernavn, Passord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        brukernavn = (EditText)findViewById(R.id.brukernavn);
-        passord = (EditText)findViewById(R.id.passord);
+        Brukernavn = (EditText)findViewById(R.id.brukernavn);
+        Passord = (EditText)findViewById(R.id.passord);
     }
 
 public void OnLogin(View view) {
-    String brukernavn = brukernavn.getText().toString();
-    String passord = passord.getText().toString();
+    String brukernavn = Brukernavn.getText().toString();
+    String passord = Passord.getText().toString();
     String type = "login";
-
     Bakgrunn bakgrunnJobb = new Bakgrunn(this);
     bakgrunnJobb.execute(type, brukernavn, passord);
 }
@@ -52,4 +71,86 @@ public void OnLogin(View view) {
         Intent i1 = new Intent(this, Registrer.class);
         startActivity(i1);
     }
+
+    public void nextAct(){
+        Intent next = new Intent(this, MainActivity.class);
+        startActivity(next);
+    }
+
+    public class Bakgrunn extends AsyncTask<String,Void,String> {
+        AlertDialog alertDialog;
+        String utTxt;
+        String Endpoint="https://web01.usn.no/~216728/api.php/";
+        public void setString(String brukernavn, String passord){
+            utTxt = Endpoint + "records/Spiller/?include=brukernavn,passord&filter=brukernavn,eq," +brukernavn + "&filter=passord,eq," + passord;
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            StringBuilder response = new StringBuilder();
+            try {
+                //System.out.println("Hei");
+                connection = (HttpURLConnection) new URL(utTxt).openConnection();
+                connection.connect();
+
+                int status = connection.getResponseCode();
+                System.out.println("Status kode er: " + status);
+                System.out.println("Hent Bruker: " + utTxt);
+                if (status == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        //response.append(reader.readLine())
+                        response.append(line);
+                        System.out.println("Appender til response: " + line);
+                    }
+
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+
+                if (connection != null)
+                    connection.disconnect();
+
+                System.out.println("Yeet1" + response.toString());
+                return response.toString();
+            }
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONObject bruker = new JSONObject();
+
+                JSONArray brukerArray = bruker.getJSONArray("records");
+                System.out.println("Bruker array: " + brukerArray.length());
+
+                loginBruker(brukerArray);
+
+            }
+            catch (JSONException e){
+                Log.e("Ugyldig JSON data", e.getMessage());
+            }
+            alertDialog.setMessage(result);
+            alertDialog.show();
+        }
+
+        public void loginBruker(JSONArray brukere){
+            if(brukere == null || brukere.length()<1){
+                Toast.makeText(Login.this, "Feil brukernavn eller passord", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(Login.this, "Du er logget inn", Toast.LENGTH_SHORT).show();
+                nextAct();
+            }
+        }
+
+
+    }
+
 }
